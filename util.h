ef2226ea38c8525c1fddee1bb5c38a9dd611c837
nokuno <nokuno@nokuno.jp>
2010-11-23 15:34:09 +0900

    implement utf8 functions to utility module

--- util.h
+++ util.h
@@ -59,6 +59,15 @@ inline vector<wstring> split_w(wstring s, wchar_t c) {
     v.push_back(s);
     return v;
 }
+//join
+inline string join(vector<string> v, char c) {
+    string result = "";
+    for (int i = 0; i < v.size(); i++) {
+        result += v[i] + c;
+    }
+    return result.substr(0, result.length()-1);
+}
+
 //join wide
 inline wstring join_w(vector<wstring> v, wchar_t c) {
     wstring result = L"";
@@ -67,6 +76,59 @@ inline wstring join_w(vector<wstring> v, wchar_t c) {
     }
     return result.substr(0, result.length()-1);
 }
+//utf-8 character length
+inline size_t utf8_char(const char *src) {
+    const unsigned char table[256] = {
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,
+        2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2, 2,2,2,2,2,2,2,2,
+        3,3,3,3,3,3,3,3, 3,3,3,3,3,3,3,3, 4,4,4,4,4,4,4,4, 4,4,4,4,4,4,4,4
+    };
+    return table[*reinterpret_cast<const unsigned char*>(src)];
+}
+//utf-8 string length
+inline size_t utf8_len(const char *src, size_t length) {
+    const char *begin = src;
+    const char *end = src + length;
+    int result = 0;
+    while (begin < end) {
+        ++result;
+        begin += utf8_char(begin);
+    }
+    return result;
+}
+//utf-8 substring
+string utf8_substring(const string &src, const size_t start, const size_t length) {
+    string result = "";
+    size_t l = start;
+    const char *begin = src.c_str();
+    const char *end = begin + src.size();
+    while (l > 0) {
+        begin += utf8_char(begin);
+        --l;
+    }
 
+    l = length;
+    while (l > 0 && begin < end) {
+        const size_t len = utf8_char(begin);
+        result.append(begin, len);
+        begin += len;
+        --l;
+    }
+    return result;
+}
+//utf-8 split string to character
+vector<string> utf8_split(string input) {
+    vector <string> result;
+    const char *p = input.c_str();
+    for (int i=0; i<input.length(); i+=utf8_char(p+i)) {
+        result.push_back(input.substr(i, utf8_char(p+i)));
+    }
+    return result;
+}
 #endif
 
