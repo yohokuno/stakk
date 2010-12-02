a88e1a99b6e3d4b5df27d50aca405bf807927534
nokuno <nokuno@nokuno.jp>
2010-12-02 13:45:53 +0900

    update unit test

--- test/connection_test.cc
+++ test/connection_test.cc
@@ -2,6 +2,7 @@
 using namespace stakk;
 
 int main(int argc, char *argv[]) {
+    try { locale::global(locale("")); } catch (...) {}
     string connection_ = "test/connection.txt";
     Connection connection;
     assert( connection.load(connection_) == true );
@@ -9,5 +10,10 @@ int main(int argc, char *argv[]) {
     assert( connection.get(0,0) == 0);
     assert( connection.get(1,3) == 5000);
     assert( connection.get(3,3) == 5000);
+
+    string id_def = "test/id.def";
+    Definition definition;
+    assert( definition.load(id_def) == true );
+    assert( definition.get(1) == L"名詞" );
     return 0;
 }
