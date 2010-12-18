568dcc58147f052ca815717c2a1d5a32488359d8
nokuno <nokuno@nokuno.jp>
2010-12-18 17:12:42 +0900

    add

---
+++ test/ttrie_test.cc
@@ -0,0 +1,49 @@
+#include "../ttrie.h"
+using namespace stakk;
+
+template <> string SimpleTrie::separator("\t");
+template <> string SimpleTrie::linebreak("\n");
+template <> string SimpleTrie::empty("");
+
+int main(int argc, char *argv[]) {
+  string filename = "test/dictionary.txt";
+  SimpleTrie trie;
+  int result = trie.load(filename, 0, '\t');
+
+  assert(result == true);
+  
+  string value = "わたし	1	1	10	私";
+
+  { // search
+    vector<string> *result = trie.search("わたし");
+    assert(result != NULL);
+    assert((*result)[0] == value);
+  }
+  { // common prefix search
+    SimpleTrie::Entries results;
+    trie.common_prefix_search("わたしのなまえ", "", results);
+    assert(results.size() != 0);
+    assert(results[0].key == "わたし");
+    assert(results[0].values.size() != 0);
+    assert(results[0].values[0] == value);
+  }
+
+  { // predictive search
+    SimpleTrie::Entries results;
+    trie.predictive_search("わた", "", results);
+    assert(results.size() != 0);
+    assert(results[0].key == "わたし");
+    assert(results[0].values.size() != 0);
+    assert(results[0].values[0] == value);
+  }
+
+  { // fuzzy search
+    SimpleTrie::Entries results;
+    trie.fuzzy_search_ex("わあし", 1, results);
+    assert(results.size() != 0);
+    assert(results[0].key == "わたし");
+    assert(results[0].values.size() != 0);
+    assert(results[0].values[0] == value);
+  }
+  return 0;
+}
